ccac18653b035dcc2715ef683260cf1cdd910b6a
Dsackler <davidesackler@gmail.com>
2023-06-12 22:50:06 -0700

    Added Proprty description and add property pop up

--- components/AddProperty.tsx
+++ components/AddProperty.tsx
@@ -1,11 +1,22 @@
-import React from 'react';
+'use client';
+import Link from 'next/link';
+import React, { useState } from 'react';
+import { PropertyDescription } from './PropertyDescription';
 
 const AddProperty = () => {
+  const [createProperty, setCreateProperty] = useState(false);
+
   return (
     <div className='flex flex-col justify-center items-center mt-40'>
-      <button className='font-semibold rounded shadow px-6 py-3 mt-20 bg-blue-600 text-neutral-100 hover:bg-blue-700'>
+      <button
+        onClick={(e) => {
+          setCreateProperty(true);
+        }}
+        className='font-semibold rounded shadow px-6 py-3 mt-20 bg-blue-600 text-neutral-100 hover:bg-blue-700'
+      >
         Add Property
       </button>
+      <div>{createProperty && <PropertyDescription />}</div>
     </div>
   );
 };
